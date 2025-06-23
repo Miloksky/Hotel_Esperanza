25380cfe3cb4fdaf5f8ccb5c81d846513a8d929a
Camilo Coral <124797907+Miloksky@users.noreply.github.com>
2025-06-23 20:05:56 +0200

    homepage updated

--- front/src/app/components/home/home.component.ts
+++ front/src/app/components/home/home.component.ts
@@ -1,27 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  selectedDate: string = '';
-  selectedGuests: number = 1;
-  availabilityMessage: string = '';
-
+  myform = inject(FormBuilder);
   guestOptions = [1, 2, 3, 4, 5, 6, 7, 8];
+  availabilityMessage: string = '';
+  today = new Date().toISOString().substring(0, 10);
+
+  dateForm: FormGroup = this.myform.group({
+    checkIn: new FormControl<string>('', Validators.required),
+    checkOut: new FormControl<string>('', Validators.required),
+    guest: new FormControl<number> (1, Validators.required)
+  });
 
   checkAvailability() {
-    if (this.selectedDate && this.selectedGuests) {
-      this.availabilityMessage = `✅ Disponibilidad para el ${this.selectedDate} con ${this.selectedGuests} persona(s).`;
-    } else {
-      this.availabilityMessage = '❗ Por favor, completa todos los campos.';
+   if(this.dateForm.invalid){
+    this.dateForm.markAllAsTouched();
+    this.availabilityMessage = 'Es necesario completar todos los campos';
+    return
+   }
+       const { checkIn, checkOut, guests } = this.dateForm.value;
+
+    if (checkIn < this.today) {
+      this.availabilityMessage = '❌ La fecha de entrada no puede ser anterior a hoy.';
+      return;
     }
+
+    if (checkIn >= checkOut) {
+      this.availabilityMessage = '❌ La fecha de salida debe ser posterior a la de entrada.';
+      return;
+    }
+   this.availabilityMessage = 'Hay disponibilidad';
+
   }
 }
