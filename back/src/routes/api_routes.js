3ae14e4340df19d9d5bc81e698d184e9b2f6d13a
Adripe24 <adriannunezcortescamacho@gmail.com>
2025-06-23 20:24:47 +0200

    .

--- back/src/routes/api_routes.js
+++ back/src/routes/api_routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 
 router.use("/user", require("./api_routes/user_routes"));
 
-router.use("/resource", require("./api_routes/resource.routes"));
+// router.use("/resource", require("./api_routes/resource.routes"));
 
 router.use("/rooms", require("./api_routes/rooms.routes"));
 
