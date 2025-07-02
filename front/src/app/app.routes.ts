87475a0e954dc0cce9dfaeeac680ea5ad6c59e47
richard <ri5ch@hotmail.com>
2025-07-02 23:44:43 +0200

    front resource

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,14 +2,20 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ReservationsComponent } from './components/reservations/reservations.component';
 import { LoginComponent } from './components/login/login.component';
-import { authGuard } from './guards/auth.guard'; 
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   {
     path: 'reservations',
     component: ReservationsComponent,
-    canActivate: [authGuard] 
+    canActivate: [authGuard]
+  },
+  {
+    path: 'resources',
+    loadComponent: () =>
+      import('./components/resources/resources.component').then(m => m.ResourcesComponent),
+    canActivate: [authGuard]
   },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
