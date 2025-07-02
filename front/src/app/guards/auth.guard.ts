93bee52830b94f48ff1318be499ddc0f5b81e0bc
Camilo Coral <124797907+Miloksky@users.noreply.github.com>
2025-07-02 22:00:58 +0200

    feat(front): solved problem of pending reservation after login

--- front/src/app/guards/auth.guard.ts
+++ front/src/app/guards/auth.guard.ts
@@ -5,6 +5,9 @@ import { AuthService } from '../services/auth.service';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+if (authService.isLoggedIn()) {
+    return true;
+  }
 
   
   const token: string | null = localStorage.getItem('token');
